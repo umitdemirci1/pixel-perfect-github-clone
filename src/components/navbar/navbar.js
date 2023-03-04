4074145e6e45af119c11f791b318d8ba3396f67d
umitd111 <umit.demirci1996@gmail.com>
2023-03-04 22:21:02 +0300

    New svgs are added. React icons is added. Most parts of nav are completed.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,50 @@
 import { NavLink } from "react-router-dom";
-
+import { ReactComponent as GithubIcon } from '../../assets/github-icon.svg'
+import { ReactComponent as NavbarSeacrhIcon } from '../../assets/navbar-search-slash.svg'
+import { ReactComponent as BellIcon } from '../../assets/bell-icon.svg'
+import { ReactComponent as PlusIcon } from '../../assets/plus-icon.svg'
+import { IoMdArrowDropdown } from 'react-icons/io'
 const Navbar = () => {
-    return ( 
+    return (
         <>
-         <NavLink to="/"> Hello </NavLink>
-            Navbar
+            <div className="flex flex-row w-full h-[60px] items-center bg-navbar-background-color px-8 py-4">
+                <div className="flex flex-row items-center text-navbar-text-color mr-4">
+                    <NavLink to="/" className="mr-4"><GithubIcon /></NavLink>
+                    <label className="
+                    w-[272px] h-7 border border-navbar-input-border-color 
+                    rounded-md bg-navbar-input-bg-color flex flex-row 
+                    justify-between items-center">
+                        <input className="
+                        bg-navbar-input-bg-color rounded-lg 
+                        placeholder-gray-300 text-sm h-full
+                        outline-none px-3"
+                            type="text" placeholder="Search or jump to..." />
+                        <NavbarSeacrhIcon className="mr-[2px]" />
+                    </label>
+                </div>
+                <div className="flex flex-row justify-between w-full mt-[2px]">
+                    <div className="text-navbar-text-color text-sm font-semibold">
+                        <NavLink to="/" className="mr-4">Pull requests</NavLink>
+                        <NavLink to="/" className="mr-4">Issues</NavLink>
+                        <NavLink to="/" className="mr-4">Codespaces</NavLink>
+                        <NavLink to="/" className="mr-4">Marketplace</NavLink>
+                        <NavLink to="/" className="mr-4">Explore</NavLink>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <BellIcon className="mr-4 hover:cursor-pointer" />
+                        <label className="flex flex-row mr-4 items-center">
+                            <PlusIcon className=" hover:cursor-pointer" />
+                            <IoMdArrowDropdown className="mt-[5px] text-white" />
+                        </label>
+                        User
+                    </div>
+                </div>
+
+            </div>
+
+
         </>
-     );
+    );
 }
- 
+
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "icons-color": "rgba(255,255,255,0.7)",
+        "navbar-background-color": "#161b22",
+        "navbar-input-border-color": "#2c3138",
+        "navbar-input-bg-color": "#0d1117",
+        "navbar-input-search-text-color": "#b5b2ae",
+        "navbar-text-color": "#f0f6fc", 
+      }
+
+    },
   },
   plugins: [],
 }
