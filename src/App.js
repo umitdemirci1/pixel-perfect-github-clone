9ee1fb95d7c705596ab6df63996d93711ed6d0ae
umitd111 <umit.demirci1996@gmail.com>
2023-03-04 21:09:38 +0300

    Route is provided. Basic folders are created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar/navbar';
+import Home from './pages/home/home';
 
 function App() {
   return (
   <>
-  
+    <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+    </Routes>
   </>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return ( 
+        <>
+         <NavLink to="/"> Hello </NavLink>
+            Navbar
+        </>
+     );
+}
+ 
+export default Navbar;
